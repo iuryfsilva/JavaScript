5c37fcc7ba706f9ca160942903cf6897147a5ff0
Iury da Fonsêca Silva <iury.silva@aluno.ufop.edu.br>
2021-03-02 09:17:45 -0400

    Comecei o modulo F  Curso em video

--- Curso em Video Js/Aula 14 Exercicios/Ex 016/script.js
+++ Curso em Video Js/Aula 14 Exercicios/Ex 016/script.js
@@ -1,4 +1,44 @@
-function contar(){
-    let inicio = document.getElementById('txtInicio');
-    let fim = document.getElementById('');
+function contar() {
+    let formularioInicio = document.getElementById('txtInicio');
+    let formularioFim = document.getElementById('txtFim');
+    let formularioPasso = document.getElementById('txtPasso');
+    let resultado = document.getElementById('result');
+
+    console.log(formularioInicio.typeOf);
+    if (formularioInicio.value.length == 0 || formularioFim.value.length == 0 || formularioPasso.value.length == 0) {
+        window.alert(`Não pode realizar a contagem`);
+    } else if (formularioPasso.value == 0) {
+        window.alert("Passo inválido considerando passo 1");
+        formularioPasso.value = 1;
+        selecionaContagem(formularioInicio, formularioFim, formularioPasso, resultado);
+    } else {
+        selecionaContagem(formularioInicio, formularioFim, formularioPasso, resultado);
+    }
+    resultado.innerHTML += "\u{1f3c1}"; 
+}
+
+function selecionaContagem(formularioInicio, formularioFim, formularioPasso, resultado) {
+    let inicio = Number(formularioInicio.value);
+    let fim = Number(formularioFim.value);
+    let passo = Number(formularioPasso.value);
+    resultado.innerHTML = ' ';
+    if (inicio > fim) {
+        contaDecrescente(inicio, fim, passo, resultado);
+    } else if (inicio < fim) {
+        contaCrescente(inicio, fim, passo, resultado);
+    }
+}
+
+function contaDecrescente(inicio, fim, passo, resultado) {
+    resultado.style.textAlign = 'center';
+    for (let control = inicio; control >= fim; control -= passo) {
+        resultado.innerHTML += `${control} \u{27A1} `;
+    }
+}
+
+function contaCrescente(inicio, fim, passo, resultado) {
+    resultado.style.textAlign = 'center';
+    for (let control = inicio; control <= fim; control += passo) {
+        resultado.innerHTML += `${control} \u{27A1} `;
+    }
 }
--- Curso em Video Js/Aula 14 Exercicios/Ex 017/script.js
+++ Curso em Video Js/Aula 14 Exercicios/Ex 017/script.js
@@ -1,13 +1,83 @@
-let agora = new Date();//acessa as informaçoes de data e hora do sistema que esta rodando o app
-var hora = agora.getHours();
-console.log(`Agora são ${hora} horas`);
-if(hora < 12){
-    window.alert("Bom dia");
-    console.log('Bom dia');
-}else if(hora <= 18){
-    window.alert("Boa tarde");
-    console.log('Boa tarde');
-}else{
-    window.alert("Boa noite");
-    console.log('Boa noite');
+function gerarTabuada() {
+    let numero = document.getElementById('txtNumero');
+    let tabuada = document.getElementById('slctTabuada');
+
+    if (numero.value.length == 0) {
+        window.alert('Número não informado!');
+    } else {
+        tabuada.innerHTML = '';
+        let valorNumero = Number(numero.value);
+        let operacao = document.getElementsByName('radOperacao');
+        
+        //Verifica qual operação foi selecionada e chama sua respectiva função de calculo
+        verificaOperacao(operacao, valorNumero, tabuada);
+    }
+}
+
+function verificaOperacao(operacao, valorNumero, tabuada){
+    if(operacao[0].checked){
+        tubuadaSoma(valorNumero, tabuada);
+    }else if(operacao[1].checked){
+        tubuadaSubtracao(valorNumero, tabuada);
+    }else if(operacao[2].checked){
+        tubuadaProduto(valorNumero, tabuada);
+    }else if(operacao[3].checked){
+        tubuadaQuociente(valorNumero, tabuada);
+    }
+}
+
+//Calcula tabuada soma
+function tubuadaSoma(valorNumero, tabuada) {
+    for (let controle = 1; controle <= 10; controle++) {
+        let item = document.createElement('option');
+        item.text = `${valorNumero} + ${controle} = ${controle + valorNumero}`;
+        
+        //para liguagem  server side esse value identifica cada valor do select
+        item.value = `tabuada${controle}`;
+        
+        //Add um elemento filho á tabuada 
+        tabuada.appendChild(item);
+    }
+}
+
+//Calcula tabuada subtracao
+function tubuadaSubtracao(valorNumero, tabuada) {
+    for (let controle = 1; controle <= 10; controle++) {
+        let item = document.createElement('option');
+        item.text = `${valorNumero} - ${controle} = ${valorNumero - controle}`;
+        
+        //para liguagem  server side esse value identifica cada valor do select
+        item.value = `tabuada${controle}`;
+        
+        //Add um elemento filho á tabuada 
+        tabuada.appendChild(item);
+    }
+}
+
+//Calcula tabuada produto
+function tubuadaProduto(valorNumero, tabuada) {
+    for (let controle = 1; controle <= 10; controle++) {
+        let item = document.createElement('option');
+        item.text = `${valorNumero} X ${controle} = ${controle * valorNumero}`;
+        
+        //para liguagem  server side esse value identifica cada valor do select
+        item.value = `tabuada${controle}`;
+        
+        //Add um elemento filho  tabuada 
+        tabuada.appendChild(item);
+    }
+}
+
+//Calcula tabuada quociente
+function tubuadaQuociente(valorNumero, tabuada) {
+    for (let controle = 1; controle <= 10; controle++) {
+        let item = document.createElement('option');
+        item.text = `${valorNumero} / ${controle} = ${valorNumero / controle}`;
+        
+        //para liguagem  server side esse value identifica cada valor do select
+        item.value = `tabuada${controle}`;
+        
+        //Add um elemento filho á tabuada 
+        tabuada.appendChild(item);
+    }
 }
